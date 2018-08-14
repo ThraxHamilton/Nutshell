e4fbbf85e7995e035e8f8ee6fb46a6874ee1cd6a
ThraxHamilton <mmcclenton16@gmail.com>
2018-08-14 15:57:04 -0500

    got user email, etc. to API

--- src/js/dataManager.js
+++ src/js/dataManager.js
@@ -1,20 +1,42 @@
 let dataManager = Object.create(null, {
     saveUserInfo: {
         value: (entry) => {
-            return fetch("http://localhost:8088/user", {
+            return fetch("http://localhost:8088/users", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(entry)
             })
-            console.log(entry)
+            console.log(saveUserInfo)
             .then(response => response.json())
         }
     },
     getUserInfo: {
         value: (entry) => {
-            return fetch("http://localhost:8088/user", {
+            return fetch("http://localhost:8088/users", {
+            })
+                .then(response => response.json())
+        }
+
+    },
+    // Save friends to API
+    saveFriendInfo: {
+        value: (entry) => {
+            return fetch("http://localhost:8088/friends", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(entry)
+            })
+            console.log(saveUserInfo)
+            .then(response => response.json())
+        }
+    },
+    getFriendInfo: {
+        value: (entry) => {
+            return fetch("http://localhost:8088/friends", {
             })
                 .then(response => response.json())
         }
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
 const dataManager = require ("./dataManager")
-
-document.querySelector("#login").addEventListener("click",(event) => {
-    // Get form field values
-    // Create object from them
-    // Add timestamp
+// Create event listener for login/register button
+document.querySelector("#register").addEventListener("click",(event) => {
     const newUser = {
+    // Using Id's defined in login.html
     email: document.querySelector("#email").value,
     username: document.querySelector("#username").value,
     password: document.querySelector("#password").value
     }
+    console.log(newUser)
+    // Save newUser to API database
     dataManager.saveUserInfo(newUser).then(() => {
     })
 })
+
